22c2f0220987cfc42f4c866cfd84fcb135a68c5d
SachinGith <88918686+SachinGith@users.noreply.github.com>
2021-12-02 15:12:55 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/portrait.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('What is your name?');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Harry Potter is cool, ' + myName + ' ,you know?';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Harry Potter is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+let myButton2 = document.querySelector('button')
+
+
